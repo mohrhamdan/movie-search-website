bb6aecedff25126bb3e16b21aa20fef23497b871
mohrhamdan <mohammadrhamdan131021@gmail.com>
2023-08-01 16:15:29 +0800

    mengambil data film dari omdbi di script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 $.ajax({
   url: "http://www.omdbapi.com/?apikey=62823513&s=avengers",
   success: (result) => {
-    console.log(result);
+    const movies = result.Search;
+    console.log(movies)
   },
-  errro: err => {
-    console.log(err.responseText)
-  }
+  errro: (err) => {
+    console.log(err.responseText);
+  },
 });
