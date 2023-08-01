821a4bdd00bad4f2298212ffa236bebd136f0f86
mohrhamdan <mohammadrhamdan131021@gmail.com>
2023-08-01 16:41:43 +0800

    membuat data dari API tampil di halaman website di file script.js

--- script.js
+++ script.js
@@ -2,7 +2,20 @@ $.ajax({
   url: "http://www.omdbapi.com/?apikey=62823513&s=avengers",
   success: (result) => {
     const movies = result.Search;
-    console.log(movies)
+    let cards = "";
+    movies.forEach((m) => {
+      cards += `  <a href="" class="card">
+                    <div class="container-image">
+                      <img src="${m.Poster}" alt="">
+                    </div>
+                    <div class="card-body">
+                      <h3>${m.Title}</h3>
+                      <p>${m.Year}</p>
+                      <button class="show-detail">Show Detail</button>
+                    </div>
+                  </a>`;
+    });
+    $(".container-cards").html(cards);
   },
   errro: (err) => {
     console.log(err.responseText);
